d71571574e5325630e57f69015a23c5017037208
LivioZ <61353261+LivioZ@users.noreply.github.com>
2022-12-11 19:59:49 +0100

    change to cleaner solution

--- day04/part1.go
+++ day04/part1.go
@@ -5,37 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
-	"strings"
 )
-
-func toInt(str string) int {
-	n, err := strconv.Atoi(str)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return n
-}
-
-// Checks if a range contains the other (interchangeably).
-//
-// Ranges are in the form of a slice.
-//
-// Example: ["82-82", "8-83"]
-func containedRanges(ranges []string) bool {
-	range1 := strings.Split(ranges[0], "-") // ["82", "82"]
-	range2 := strings.Split(ranges[1], "-") // ["8", "83"]
-	n1 := toInt(range1[0])
-	n2 := toInt(range1[1])
-	n3 := toInt(range2[0])
-	n4 := toInt(range2[1])
-
-	/*
-			n1---n2		 n1-n2
-			 n3-n4		n3---n4
-	*/
-	return (n3 >= n1 && n4 <= n2) || (n1 >= n3 && n2 <= n4)
-}
 
 func Part1() {
 	sum := 0
@@ -44,17 +14,19 @@ func Part1() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer input.Close()
 
 	scanner := bufio.NewScanner(input)
 
 	for scanner.Scan() {
-		line := scanner.Text() // "82-82,8-83"
-		ranges := strings.Split(line, ",") // ["82-82", "8-83"]
-		
-		if containedRanges(ranges) {
+		var startFirst, endFirst, startSecond, endSecond int
+		// "82-82,8-83"
+		fmt.Sscanf(scanner.Text(), "%d-%d,%d-%d", &startFirst, &endFirst, &startSecond, &endSecond)
+
+		if (startSecond >= startFirst && endSecond <= endFirst) ||
+			(startFirst >= startSecond && endFirst <= endSecond) {
 			sum++
 		}
 	}
-
 	fmt.Printf("%v assignment pairs fully contain the other\n", sum)
 }
--- day04/part2.go
+++ day04/part2.go
@@ -5,48 +5,28 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
 )
 
-// Checks if two ranges overlap.
-//
-// Ranges are in the form of a slice.
-//
-// Example: ["82-82", "8-83"]
-func overlappingRanges(ranges []string) bool {
-	range1 := strings.Split(ranges[0], "-") // ["82", "82"]
-	range2 := strings.Split(ranges[1], "-") // ["8", "83"]
-	n1 := toInt(range1[0])
-	n2 := toInt(range1[1])
-	n3 := toInt(range2[0])
-	n4 := toInt(range2[1])
-
-	/*
-			n1---n2		 n1-n2
-			 n3-n4		n3---n4
-	*/
-	return (((n1<=n3 && n3<=n2) || (n1<=n4 && n4<=n2)) ||
-		    ((n3<=n1 && n1<=n4) || (n3<=n2 && n2<=n4)))
-}
-
 func Part2() {
-	sum := 0
+	numberOfOverlaps := 0
 
 	input, err := os.Open("day04/input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer input.Close()
 
 	scanner := bufio.NewScanner(input)
 
 	for scanner.Scan() {
-		line := scanner.Text() // "82-82,8-83"
-		ranges := strings.Split(line, ",") // ["82-82", "8-83"]
-		
-		if overlappingRanges(ranges) {
-			sum++
+		var startFirst, endFirst, startSecond, endSecond int
+
+		fmt.Sscanf(scanner.Text(), "%d-%d,%d-%d", &startFirst, &endFirst, &startSecond, &endSecond)
+
+		if (startSecond <= endFirst && endSecond >= startFirst) ||
+			(startFirst <= endSecond && endFirst >= startSecond) {
+			numberOfOverlaps++
 		}
 	}
-
-	fmt.Printf("%v assignment pairs overlap\n", sum)
+	fmt.Printf("%v assignment pairs overlap\n", numberOfOverlaps)
 }
