093d54caec7a60aea6777bafd9bfc3f48108e008
LivioZ <61353261+LivioZ@users.noreply.github.com>
2022-12-22 14:19:29 +0100

    better solution

--- day13/part2.go
+++ day13/part2.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 )
 
 func Part2() {
@@ -42,44 +43,23 @@ func Part2() {
 	}
 
 	// add divider packets
-	packets = append(packets, []any{[]any{float64(2)}}, []any{[]any{float64(6)}})
+	div1 := []any{[]any{float64(2)}}
+	div2 := []any{[]any{float64(6)}}
+	packets = append(packets, div1, div2)
 
-	// insertion sort
-	for i := 1; i < len(packets); i++ {
-		v := packets[i]
-		j := i - 1
-		for j >= 0 && !less(packets[j], v) {
-			packets[j+1] = packets[j]
-			j = j - 1
-		}
-		packets[j+1] = v
-	}
+	// sort packets
+	sort.Slice(packets, func(i, j int) bool {
+		return isOrdered(packets[i], packets[j]) == 1
+	})
 
 	res := 1
 
-	for i, packet := range packets {
-		if p, ok := packet.([]any); ok {
-			if len(p) == 1 {
-				if p1, ok := p[0].([]any); ok {
-					if len(p1) == 1 {
-						if p2, ok := p1[0].(float64); ok {
-							if p2 == 2.0 || p2 == 6.0 {
-								res *= i + 1
-							}
-						}
-					}
-				}
-			}
+	// find dividers
+	for i, p := range packets {
+		if isOrdered(p, div1) == 0 || isOrdered(p, div2) == 0 {
+			res *= i + 1
 		}
 	}
 
 	fmt.Printf("Product of indexes of divider packets after sorting: %v\n", res)
-}
-
-func less(left, right any) bool {
-	if isOrdered(left, right) == 1 {
-		return true
-	} else {
-		return false
-	}
 }
